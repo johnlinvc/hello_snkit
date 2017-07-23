65734325dc6b4e03b13b7111f363e0ae0ac00f5d
johnlinvc <johnlinvc@gmail.com>
2017-07-24 00:52:30 +0800

    can show the buttons

--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -6,9 +6,17 @@ import Foundation
 #endif
 
 class ViewController : SNViewController {
+    var label:SNLabel?
     override func viewDidLoad() {
-        let label = SNLabel(frame:CGRect(x: 100, y: 100, width: 100, height: 100))
+        label = SNLabel(frame:CGRect(x: 100, y: 100, width: 100, height: 100))
         label.text = "hello SNKit"
         self.view.addSubview(label)
+        let button = SNButton(frame:CGRect(x: 200, y: 200, width: 120, height: 40))
+        button.setTitle("Magic Button", forState: .Normal)
+        self.view.addSubview(button)
+    }
+
+    func buttonPushed() {
+        label.text = "Someone pushed the button"
     }
 }
