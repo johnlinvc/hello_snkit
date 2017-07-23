fac55b46d49a8c405ddfa69424a94e3767bfc96e
johnlinvc <johnlinvc@gmail.com>
2017-07-23 17:14:43 +0800

    update Packages

--- Package.swift
+++ Package.swift
@@ -3,5 +3,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "hello_snkit"
+    name: "hello_snkit",
+    dependencies: [
+      .Package(url: "../SNKit", majorVersion: 0),
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7)
+    ]
 )
