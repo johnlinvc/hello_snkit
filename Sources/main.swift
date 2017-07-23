877c689557d398272f8f2d5dd0a8d6e960f91226
johnlinvc <johnlinvc@gmail.com>
2017-07-23 17:19:03 +0800

    add basic hello world with Kitura

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,12 @@
-print("Hello, world!")
+import Kitura
+
+let router = Router()
+router.get("/") {
+    req, res, next in
+    res.send("Hello, world")
+    next()
+}
+
+
+Kitura.addHTTPServer(onPort: 8080, with: router)
+Kitura.run()
