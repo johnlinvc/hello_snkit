1d5b82210397f5ce9a6fc2c5dadd69417ba59478
johnlinvc <johnlinvc@gmail.com>
2017-07-25 11:30:00 +0800

    change SNKit repo url

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "hello_snkit",
     dependencies: [
-      .Package(url: "../SNKit", majorVersion: 0),
+      .Package(url: "https://github.com/johnlinvc/SNKit", majorVersion: 0),
       .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7)
     ]
 )
