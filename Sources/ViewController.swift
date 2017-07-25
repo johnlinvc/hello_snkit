29e9f1c21b4e23308bcf8cc16b62275b3b7ba292
johnlinvc <johnlinvc@gmail.com>
2017-07-25 11:13:59 +0800

    change for latest sdk

--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -9,12 +9,13 @@ class ViewController : SNViewController {
     var label:SNLabel!
     override func viewDidLoad() {
         label = SNLabel(frame:CGRect(x: 100, y: 100, width: 100, height: 100))
-        label!.text = "hello SNKit"
+        label.text = "hello SNKit"
         self.view.addSubview(label!)
-        let button = SNButton(frame:CGRect(x: 200, y: 200, width: 120, height: 40))
-        button.setTitle("Magic Button", forState: .Normal)
+        let button = SNButton(frame:CGRect(x: 200, y: 200, width: 200, height: 40))
+        button.setTitle("Magic Button", for: .normal)
+        button.backgroundColor = SNColor.blue
         self.view.addSubview(button)
-        button.addTarget(self, action: #selector(ViewController.buttonPushed), forControlEvents: .TouchUpInside)
+        button.addTarget(self, action: #selector(ViewController.buttonPushed), for: .touchUpInside)
     }
 
     @objc
